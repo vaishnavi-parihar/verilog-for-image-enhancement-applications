8b0f0546eb5fbe9ef34858094f7f2b9997a7fcdd
vaishnavi-parihar <75555001+vaishnavi-parihar@users.noreply.github.com>
2023-04-18 22:16:28 +0530

    add parameter file

---
+++ parameter.v
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+//************** DEFINITION FILES ******************************************
+//---------------------------------------------------------------------------
+
+`define INPUT_FILE "inputimage.hex"   // INPUT HEX file name
+`define OUTPUT_FILE "outputimage.bmp"   // OUTPUT BMP file name
+
+//---------------------------------------------------------------------------
+//----------------  LIST OF DIFFERENT OPERATIONS ----------------------------
+//---------------------------------------------------------------------------
+
+`define ORIGINAL_IMAGE 
+//desired operation of code can used by deleting '//' at the beginning of the selected line
+
+//`define INVERT_OPERATION
+
+//`define BRIGHTNESS_OPERATION
+
+//`define THRESHOLD_OPERATION
